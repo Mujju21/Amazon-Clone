69bbf7c64e7a959a6f7957e74568ae6c76f93a3c
Mujamill Shaikh <98961526+Mujju21@users.noreply.github.com>
2025-01-12 00:31:25 +0530

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,8 @@
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import{renderOrderSummary} from './checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import '../data/backend_practice.js';
+
+renderCheckoutHeader();
+renderOrderSummary();
+renderPaymentSummary();
